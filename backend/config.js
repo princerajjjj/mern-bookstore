396b818a4663d4312b864d490307ac331bd6a223
Prince <princerajput192002@gmail.com>
2024-04-01 17:46:16 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://princerajput192002:d74palGpMoOZgW5x@book-store-db.lv94eci.mongodb.net/books-collection?retryWrites=true&w=majority&appName=book-store-db';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+ import booksRoute from './routes/booksRoute.js';
+ import cors from 'cors';
+import {Book} from './models/bookModel.js';
+
+const app = express();
+
+
+// princerajput192002    d74palGpMoOZgW5x
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+ app.use(cors());
+
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
+  app.use('/books', booksRoute);
+
+ mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+     console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+     console.log(error);
+   });
